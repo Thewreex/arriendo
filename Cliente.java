330770a7d35297254a8cd018b6b4557053b5787c
JuanPablo <juanpablodiaz2518@gmail.com>
2026-03-23 23:56:45 -0300

    Setters creados

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,19 @@
 public class Cliente {
+    private String cedula;
+    private String nombre;
+    private boolean vigente;
 
+    public Cliente(String cedula, String nombre) {
+        // setCedula(cedula)
+        // setNombre(nombre)
+        this.vigente = true;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
