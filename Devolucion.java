ef9074682c5b4e7838997534268bd50598e7e885
Bastiaan3004 <bastianbastidas8@gmail.com>
2026-03-24 13:11:46 -0300

    Update Devolucion.java

--- Devolucion.java
+++ Devolucion.java
@@ -1,3 +1,21 @@
-public class Devolucion {
 
+import java.time.LocalDate;
+
+public class Devolucion {
+    private Arriendo arriendo;
+    private LocalDate fechaDevolucion;
+
+    public Devolucion(Arriendo arriendo, String fechaDevolucion) {
+        this.arriendo = arriendo;
+        LocalDate fechaDev = LocalDate.parse(fechaDevolucion);
+
+        if (!fechaDev.isBefore(arriendo.getFechaArriendo())) {
+            this.fechaDevolucion = fechaDev;
+        } else {
+            throw new IllegalArgumentException("Fecha inválida");
+        }
+    }
+
+    public Arriendo getArriendo() { return arriendo; }
+    public LocalDate getFechaDevolucion() { return fechaDevolucion; }
 }
