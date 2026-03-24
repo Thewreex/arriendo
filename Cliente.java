51ec6a778f7480577f4926cce4c9ec477d699e0d
Bastiaan3004 <bastianbastidas8@gmail.com>
2026-03-24 13:03:41 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,20 @@
-public class Cliente {
 
+public class Cliente {
+    private String cedula;
+    private String nombre;
+    private boolean vigencia;
+
+    public Cliente(String cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.vigencia = true;
+    }
+
+    public String getCedula() { return cedula; }
+    public String getNombre() { return nombre; }
+    public boolean isVigente() { return vigencia; }
+
+    public void setVigencia(boolean vigencia) {
+        this.vigencia = vigencia;
+    }
 }
