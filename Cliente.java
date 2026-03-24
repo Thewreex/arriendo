983568b8ce90398ea33ed1a0b703811b9f8d3ded
JuanPablo <juanpablodiaz2518@gmail.com>
2026-03-24 00:17:01 -0300

    Validacion del campo rut

--- Cliente.java
+++ Cliente.java
@@ -4,16 +4,32 @@ public class Cliente {
     private boolean vigente;
 
     public Cliente(String cedula, String nombre) {
-        // setCedula(cedula)
-        // setNombre(nombre)
+        setCedula(cedula);
+        setNombre(nombre);
         this.vigente = true;
     }
 
     public void setCedula(String cedula) {
-        this.cedula = cedula;
+        if (validarCedula(cedula)) {
+            this.cedula = cedula.toUpperCase();
+        }
+    }
+
+    public boolean validarCedula(String cedula) {
+        if (cedula == null || cedula.length() != 10) {
+            return false;
+        }
+
+        if (cedula.charAt(8) != '-') {
+            return false;
+        }
+
+        char ultDigito = cedula.charAt(9);
+        return (ultDigito >= '0' && ultDigito <= '9') || (ultDigito == 'k' || ultDigito == 'K');
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
 }
